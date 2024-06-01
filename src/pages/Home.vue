9bb38da0f77616e326abc1486bda29b087b8a43e
JumanJi <90971841+heyjumanji@users.noreply.github.com>
2024-06-02 03:22:15 +0530

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,10 @@
   <div id="box">
     <wallpapers/>
     <div class="header">
-      <a href="https://github.com/heyjumanji/host-image" target="_blank"></a>
+      <a href="https://github.com/heyjumanji/host-image" target="_blank" class="icon github"></a>
+      <!-- Removed Twitter icon -->
+      <!-- <a href="https://twitter.com/example" target="_blank" class="icon twitter"></a> -->
+      <a href="https://x.com" target="_blank" class="icon xcom"></a>
     </div>
     <div class="main">
       <Upload/>
@@ -12,13 +15,14 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import Wallpapers from "../components/Wallpapers.vue"
 import Upload from "../components/Upload.vue"
 </script>
 
 <style scoped lang="less">
-#box{
+#box {
   display: flex;
   flex-direction: column;
   height: 100vh;
@@ -26,35 +30,41 @@ import Upload from "../components/Upload.vue"
   position: relative;
   box-sizing: border-box;
   padding: 30px;
-  background: linear-gradient(240deg,rgba(150,50,50,.3),rgba(0,0,200,0));
-  .main{
-    flex: 1;
-    flex-direction: column;
-    padding: 30px 0;
-    align-items: center;
-    box-sizing: border-box;
-  }
-  .header{
-    display: flex;
-    justify-content: end;
-    a{
-      width: 22px;
-      height: 22px;
-      background-image: url("/github.svg");
-      background-color: transparent;
-      background-size: 110%;
-      background-repeat: no-repeat;
-      background-position: top;
-    }
-  }
-  .footer{
-    font-size: 12px;
-    color: #fff;
-    a{
-      color: #fff;
-      text-decoration: none;
-      font-weight: 700
-    }
-  }
+  background: linear-gradient(240deg, rgba(150, 50, 50, .3), rgba(0, 0, 200, 0));
+}
+.header {
+  display: flex;
+  justify-content: end;
+}
+.icon {
+  width: 22px;
+  height: 22px;
+  background-color: transparent;
+  background-size: 110%;
+  background-repeat: no-repeat;
+  background-position: top;
+}
+.github {
+  background-image: url("/github.svg");
+}
+/* Removed Twitter icon */
+.xcom {
+  background-image: url("/xcom.svg");
+}
+.main {
+  flex: 1;
+  flex-direction: column;
+  padding: 30px 0;
+  align-items: center;
+  box-sizing: border-box;
+}
+.footer {
+  font-size: 12px;
+  color: #fff;
+}
+.footer a {
+  color: #fff;
+  text-decoration: none;
+  font-weight: 700;
 }
 </style>
