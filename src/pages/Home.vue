d8185e45c08bd245a8e57ab907473a85e12c71a6
JumanJi <90971841+heyjumanji@users.noreply.github.com>
2024-06-02 02:07:02 +0530

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,7 +8,7 @@
       <Upload/>
     </div>
     <div class="footer">
-      based on <a href="https://telegra.ph" target="_blank">Telegraph</a> 的图片上传工具，UI来自 <a href="https://github.com/cf-pages/Telegraph-Image" target="_blank">Telegraph-Image</a>
+      based on <a href="https://telegra.ph" target="_blank">Telegraph</a> Image upload tool, UI comes from <a href="https://github.com/cf-pages/Telegraph-Image" target="_blank">Telegraph-Image</a>
     </div>
   </div>
 </template>
