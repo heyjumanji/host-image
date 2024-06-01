70dc126b7bbf6d4ca6e9873941c5c6bd560e5c5b
JumanJi <90971841+heyjumanji@users.noreply.github.com>
2024-06-02 02:18:21 +0530

    Update Upload.vue

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="upload">
-    <div class="title">Telegraph-Image-Pages</div>
+    <div class="title">host-image</div>
     <div class="upload-main">
         <Waiting v-show="status === 0" @change="onChange" :show="status === 0"/>
         <Uploading v-show="status === 1"/>
