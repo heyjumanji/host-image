db84e797028254c5dafdad0ecb28ecf57502a659
JumanJi <90971841+heyjumanji@users.noreply.github.com>
2024-06-02 20:16:00 +0530

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="box">
-    <div class="custom-background"></div>
+    <wallpapers/>
     <div class="header">
-      <a href="https://github.com/heyjumanji" target="_blank"></a>
+      <a href="https://github.com/heyjumanji/host-image" target="_blank"></a>
     </div>
     <div class="main">
-      <Upload @imageUploaded="handleImageUploaded"/>
+      <Upload/>
+    </div>
+    <div class="footer">
+      based on <a href="https://telegra.ph" target="_blank">Telegraph</a> Image upload tool, UI comes from<a href="https://github.com/cf-pages/Telegraph-Image" target="_blank">Telegraph-Image</a>
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
 import Wallpapers from "../components/Wallpapers.vue"
 import Upload from "../components/Upload.vue"
-import { reactive } from "vue";
-
-// Function to handle image uploaded event
-const handleImageUploaded = (imageUrl: string) => {
-  // Handle image upload here
-}
 </script>
 
 <style scoped lang="less">
-#box {
+#box{
   display: flex;
   flex-direction: column;
   height: 100vh;
@@ -30,40 +26,35 @@ const handleImageUploaded = (imageUrl: string) => {
   position: relative;
   box-sizing: border-box;
   padding: 30px;
-}
-
-.custom-background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url('https://host-image.pages.dev/file/7029545f9e32738b1b361.jpg'); /* Replace '/path/to/your/image.jpg' with the URL of your image */
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-}
-
-.main {
-  flex: 1;
-  flex-direction: column;
-  padding: 30px 0;
-  align-items: center;
-  box-sizing: border-box;
-}
-
-.header {
-  display: flex;
-  justify-content: end;
-}
-
-.header a {
-  width: 22px;
-  height: 22px;
-  background-image: url("/github.svg");
-  background-color: transparent;
-  background-size: 110%;
-  background-repeat: no-repeat;
-  background-position: top;
+  background: linear-gradient(240deg,rgba(150,50,50,.3),rgba(0,0,200,0));
+  .main{
+    flex: 1;
+    flex-direction: column;
+    padding: 30px 0;
+    align-items: center;
+    box-sizing: border-box;
+  }
+  .header{
+    display: flex;
+    justify-content: end;
+    a{
+      width: 22px;
+      height: 22px;
+      background-image: url("/github.svg");
+      background-color: transparent;
+      background-size: 110%;
+      background-repeat: no-repeat;
+      background-position: top;
+    }
+  }
+  .footer{
+    font-size: 12px;
+    color: #fff;
+    a{
+      color: #fff;
+      text-decoration: none;
+      font-weight: 700
+    }
+  }
 }
 </style>
